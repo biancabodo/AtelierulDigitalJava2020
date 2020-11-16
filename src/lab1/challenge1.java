e045f628fe0f044e450d1cac26f01c24056f6d04
Bianca <71593628+biancabodo@users.noreply.github.com>
2020-11-16 21:35:09 +0200

    lab1

--- src/lab1/challenge1.java
+++ src/lab1/challenge1.java
@@ -1,21 +1,22 @@
-public class aiae {
+package lab1;
+
+public class challenge1 {
     public static void main(String[] args) {
         System.out.println("Challenge 1");
 
         for(int i = 1; i<=100;i++){
-            if((i%3 != 0) && (i%5!=0)){
+            if((i%7 != 0) && (i%11!=0)){
                 System.out.print(i);
 
             }
-            else if (i % 3 == 0){
-                System.out.print("bizz");
+            else if (i % 7 == 0){
+                System.out.print("Rizz");
             }
-            else if (i%5==0){
-                System.out.print("razz");
+            else if (i%11==0){
+                System.out.print("Jazz");
             }
 
             System.out.print(", ");
         }
     }
 }
-
