9caf424eb9b45a63f7ebe9264a939481ef4afc2b
Bianca <71593628+biancabodo@users.noreply.github.com>
2020-11-22 20:18:29 +0200

    Challenge3

---
+++ src/lab1/challenge3.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class challenge3 {
+    public static void main(String[] args) {
+       int[] array = {3,2,-3,-2,3,0};
+       int counter = 0;
+
+       for(int i = 0; i<array.length-1;i++){
+           for(int j = i+1; j<array.length;j++){
+               if ((array[i] + array[j] == 0) && (array[i] !=  0) && (array[j] != 0)){
+                   counter += 1;
+                   array[i] = array[j] = 0;
+               }
+           }
+       }
+        System.out.print(counter);
+
+    }
+}
